1114d4f4bf196e927da0b782e659b49f41957176
Jake Wharton <jw@squareup.com>
2012-06-02 01:17:44 -0700

    My solution.

--- src/main/java/com/squareup/jobs/JspParser.java
+++ src/main/java/com/squareup/jobs/JspParser.java
@@ -1,6 +1,14 @@
 package com.squareup.jobs;
 
 public class JspParser {
+  private static final int STATE_ACCEPTING_STRING = 0;
+  private static final int STATE_MIGHT_BE_JSP_OPEN_TAG = 1;
+  private static final int STATE_MIGHT_BE_JSP_PRINT_TAG = 2;
+  private static final int STATE_ACCEPTING_JSP = 3;
+  private static final int STATE_MIGHT_BE_JSP_CLOSE_TAG = 4;
+
+  private static final int TYPE_JAVA = 0;
+  private static final int TYPE_PRINT = 1;
 
   /**
    * JSP Tags:
@@ -24,8 +32,73 @@ public class JspParser {
    * System.out.print("</p>  </body></html>");
    */
   public static String parse(String jsp) {
-    // TODO Implement me!
-    return "";
+    final StringBuilder java = new StringBuilder("System.out.print(\"");
+
+    int state = STATE_ACCEPTING_STRING;
+    int type = -1;
+    for (int i = 0; i < jsp.length(); i++) {
+      final char current = jsp.charAt(i);
+
+      switch (state) {
+
+        case STATE_ACCEPTING_STRING:
+          if ('<' == current) {
+            state = STATE_MIGHT_BE_JSP_OPEN_TAG;
+          } else {
+            java.append(current);
+          }
+          break;
+
+        case STATE_MIGHT_BE_JSP_OPEN_TAG:
+          if ('%' == current) {
+            state = STATE_MIGHT_BE_JSP_PRINT_TAG;
+            java.append("\");"); // Close string output
+          } else {
+            state = STATE_ACCEPTING_STRING;
+            java.append('<').append(current);
+          }
+          break;
+
+        case STATE_MIGHT_BE_JSP_PRINT_TAG:
+          if ('=' == current) {
+            type = TYPE_PRINT;
+            java.append("System.out.print("); // Start evaluation output
+          } else {
+            type = TYPE_JAVA;
+            java.append(current);
+          }
+          state = STATE_ACCEPTING_JSP;
+          break;
+
+        case STATE_ACCEPTING_JSP:
+          if ('%' == current) {
+            state = STATE_MIGHT_BE_JSP_CLOSE_TAG;
+          } else {
+            java.append(current);
+          }
+          break;
+
+        case STATE_MIGHT_BE_JSP_CLOSE_TAG:
+          if ('>' == current) {
+            switch (type) {
+              case TYPE_JAVA:
+                java.append(";");
+                break;
+              case TYPE_PRINT:
+                java.append(");");
+                break;
+            }
+            state = STATE_ACCEPTING_STRING;
+            java.append("System.out.print(\"");
+          } else {
+            java.append('%').append(current);
+            state = STATE_ACCEPTING_JSP;
+          }
+          break;
+      }
+    }
+
+    return java.append("\");").toString();
   }
 
   public static void main(String... args) {
@@ -41,7 +114,7 @@ public class JspParser {
         + "    <p><code>i = <%=i%></code> and that's 33% more than <code>j = <%=j%></p>"
         + "  </body>"
         + "</html>";
-    //jsp += "<!-- <%=\"I'm in ur strings! <% <%= % > %> \"%> -->";
+    jsp += "<!-- <%=\"I'm in ur strings! <% <%= % > \"%> -->";
 
     System.out.println(parse(jsp));
   }
