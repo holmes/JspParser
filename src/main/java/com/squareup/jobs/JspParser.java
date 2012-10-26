0204b9de61446b72e55ee8852c8aacac03ed7962
Jason Holmes <holmes@squareup.com>
2012-10-26 14:15:17 -0700

    Introduce the states and the switch

--- src/main/java/com/squareup/jobs/JspParser.java
+++ src/main/java/com/squareup/jobs/JspParser.java
@@ -1,21 +1,44 @@
 package com.squareup.jobs;
 
 public class JspParser {
+  private static final int STATE_ACCEPTING_STRING = 0;
+  private static final int STATE_MIGHT_BE_JSP_OPEN_TAG = 1;
+  private static final int STATE_MIGHT_BE_JSP_PRINT_TAG = 2;
+  private static final int STATE_ACCEPTING_JSP = 3;
+  private static final int STATE_MIGHT_BE_JSP_CLOSE_TAG = 4;
+
+  private static final int TYPE_JAVA = 0;
+  private static final int TYPE_PRINT = 1;
 
   /**
    * JSP Tags:
    *
-   *  <% xyz %>
+   * <xyz> where the whole tag is passed to System.out.println
    *
-   * where "xyz" is pure Java code
+   * <% xyz %> where "xyz" is pure Java code
    *
-   *  <%= xyz %>
-   *
-   * where "xyz" is passed to System.out.println.
+   * <%= xyz %> where "xyz" is passed to System.out.println.
    */
   public static String parse(String jsp) {
-    // TODO Implement me!
-    return "";
+    final StringBuilder java = new StringBuilder("System.out.print(\"");
+
+    int state = STATE_ACCEPTING_STRING;
+    int type = -1;
+
+    switch (state) {
+      case STATE_ACCEPTING_STRING:
+        break;
+      case STATE_MIGHT_BE_JSP_OPEN_TAG:
+        break;
+      case STATE_MIGHT_BE_JSP_PRINT_TAG:
+        break;
+      case STATE_ACCEPTING_JSP:
+        break;
+      case STATE_MIGHT_BE_JSP_CLOSE_TAG:
+        break;
+    }
+
+    return java.append("\");").toString();
   }
 
   public static void main(String... args) {
@@ -35,5 +58,4 @@ public class JspParser {
 
     System.out.println(parse(jsp));
   }
-
 }
